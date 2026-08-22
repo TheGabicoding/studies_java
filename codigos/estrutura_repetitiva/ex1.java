4355a01eeda2dc170355445135316692bca87628
TheGabicoding <gabriel23gomes11@gmail.com>
2026-08-22 18:49:02 -0300

    ex2 added

--- codigos/estrutura_repetitiva/ex1.java
+++ codigos/estrutura_repetitiva/ex1.java
@@ -1,5 +1,23 @@
 package codigos.estrutura_repetitiva;
-
+import java.util.Scanner;
 public class ex1 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int senha = 2002;
+
+        int valor = sc.nextInt();
+        while (true){
+            if (valor == senha){
+                System.out.println("Acesso Permitido");
+                break;
+            }
+            else{
+            System.out.println("Senha Invalida");
+            valor = sc.nextInt();
+        }
+        }
+
+        sc.close();
+    }
 }
